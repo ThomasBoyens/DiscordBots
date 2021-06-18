f88be817ebd9cc5e70911737a191ac3ff541ebd2
thomas boyens <thomas.boyens@hotmail.com>
2021-06-18 20:32:34 +0200

    bigbenbot

---
+++ BigBenBot/main.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+
+const client = new Discord.Client();
+
+const prefix = '~';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    console.log('Big Ben Bot is online!');
+    var now = new Date();
+    var minutes = now.getMinutes();
+    console.log(minutes)
+    //if (minutes === 0) {
+        const channel = client.channels.cache.get("705135370861281335");
+        if (!channel) return console.error("The channel does not exist!");
+        channel.join().then(connection => {
+            // Yay, it worked!
+            console.log("Successfully connected.");
+        }).catch(e => {
+            // Oh no, it errored! Let's log it to console :)
+            console.error(e);
+        });
+    //}
+});
+
+
+
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        client.commands.get('ping').execute(message, args);
+    }
+});
+
+
+client.login('ODU1NDU2NDkyOTI0NTY3NTgy.YMyv8Q.-acqUfnZMOIt9mArwl2_cXi4RWk');
